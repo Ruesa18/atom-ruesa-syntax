5a709430c22a69097c0dd9bae237935e751cd7a8
Sandro Rüfenacht <sandro.ruefenacht0@gmail.com>
2021-09-03 21:56:05 +0200

    Initial commit

---
+++ test.php
@@ -0,0 +1,16 @@
+<?php
+
+    /**
+     * [test description]
+     *
+     * @param  [type] $parameter [description]
+     * @return [type]            [description]
+     */
+    function test($parameter) {
+        return "LOL";
+    }
+
+    echo "Absolut lol";
+    $var = test("PE");
+    var_dump(is_bool($var));
+?>
